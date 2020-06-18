7d1bfb02e35a6791d63e1e1a829eeeb4c3089b3a
Ben Irvine <benji72027@gmail.com>
2020-06-18 17:55:15 +1200

    Create damnify css and jsx files

---
+++ client/components/Damnify.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Damnify extends React.Component {
+    render() {
+
+
+        return (
+            <>
+                <img href=""></img>
+            </>
+    )
+    }
+
+}
+
+export default Damnify
