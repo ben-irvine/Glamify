3e8d68c1056f5976474662e2d5630ede5c124186
Ben Irvine <benji72027@gmail.com>
2020-06-19 11:26:06 +1200

    Glitch animation css

--- client/components/Damnify.jsx
+++ client/components/Damnify.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
+
+
 class Damnify extends React.Component {
     render() {
-
-
         return (
             <>
-                <img src={this.props.imageURL}></img>
+                <img id='img' src={this.props.imageURL}></img>
             </>
     )
     }
-
 }
 
 export default Damnify
