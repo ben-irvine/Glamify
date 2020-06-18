788c87ffd6badfc364d12812d192a991e41dda7f
Nick-Jarvie <nickjarvie@gmail.com>
2020-06-18 17:52:48 +1200

    Add some form stuff

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    imageURL: '',
+  }
+
+  handleSubmit = event => {
+    // Don't automatically submit the form
+    event.preventDefault()
+
+    console.log('submit', this.state)
+
+    this.props.setName(this.state.name)
+  }
+
+  handleChange = event => {
+    console.log('change!', event.target.name)
+
+    // const newState = {}
+    // newState[event.target.name] = event.target.value
+    // this.setState(newState)
+
+    // This ---/\ is the same as this ----\/
+
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <label>
+          Input image link:
+          <input type="text" name="image-link" onChange={this.handleChange}/>
+        </label>
+
+        <input type="submit" value="Save" />
+      </form>
+    )
+  }
 }
+
+
 
 export default App
